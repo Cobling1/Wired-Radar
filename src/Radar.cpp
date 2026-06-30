982623a3a8d39ff6c96e15d8834db3ac77f37fae
Cobling1 <cob@live.com.au>
2026-07-01 03:47:50 +1000

    Integrate Renderer into Radar for drawing
    Refactor Radar class to use Renderer for drawing operations.

--- src/Radar.cpp
+++ src/Radar.cpp
@@ -1,10 +1,18 @@
 #include "Radar.h"
+
+#include "../graphics/Renderer.h"
 #include "../platform/Display.h"
 #include "../Config.h"
 
-void Radar::begin(Display* d)
+static Renderer renderer;
+
+void Radar::begin(Display *display)
 {
-    display = d;
+    m_display = display;
+
+    renderer.begin(display);
+
+    m_renderer = &renderer;
 }
 
 void Radar::update()
@@ -14,46 +22,75 @@ void Radar::update()
 
 void Radar::draw()
 {
-    display->clear();
+    m_renderer->clear();
 
-    display->drawCircle(
-        240,
-        240,
-        RADAR_RADIUS,
-        0x07E0);
+    drawRangeRings();
 
-    display->drawCircle(
-        240,
-        240,
-        RADAR_RADIUS/2,
-        0x07E0);
+    drawCompass();
 
-    display->drawLine(
-        240,
-        30,
-        240,
-        450,
-        0x07E0);
+    drawCenter();
 
-    display->drawLine(
-        30,
-        240,
-        450,
-        240,
-        0x07E0);
+    drawScale();
 
-    display->fillCircle(
+    m_renderer->present();
+}
+
+void Radar::drawRangeRings()
+{
+    constexpr uint16_t colour = 0x03E0;
+
+    m_renderer->circle(240,240,200,colour);
+    m_renderer->circle(240,240,150,colour);
+    m_renderer->circle(240,240,100,colour);
+    m_renderer->circle(240,240,50,colour);
+}
+
+void Radar::drawCompass()
+{
+    constexpr uint16_t colour = 0x03E0;
+
+    m_renderer->line(40,240,440,240,colour);
+
+    m_renderer->line(240,40,240,440,colour);
+
+    m_renderer->text(235,8,"N",0xFFFF,2);
+
+    m_renderer->text(235,452,"S",0xFFFF,2);
+
+    m_renderer->text(8,232,"W",0xFFFF,2);
+
+    m_renderer->text(452,232,"E",0xFFFF,2);
+}
+
+void Radar::drawCenter()
+{
+    m_renderer->fillCircle(
         240,
         240,
         6,
         0xFFFF);
 
-    display->drawText(
-        230,
-        10,
-        "N",
+    m_renderer->line(
+        240,
+        220,
+        240,
+        205,
+        0xFFFF);
+}
+
+void Radar::drawScale()
+{
+    char txt[20];
+
+    sprintf(
+        txt,
+        "%.0f NM",
+        ZOOM_LEVELS[zoomIndex]);
+
+    m_renderer->text(
+        185,
+        25,
+        txt,
         0xFFFF,
         2);
-
-    display->present();
 }
